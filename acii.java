1c5b0f4ba38d8cb351f2d2187ffe604a86414bab
Aksharakeerthi <akshara2223@gmail.com>
2017-07-18 09:44:36 +0530

    Create acii.java

---
+++ acii.java
@@ -0,0 +1,11 @@
+# acii
+import java.io.*;
+import java.util.*;
+public class acii{
+public static void main(String args[]){
+int i;
+for(i=0;i<26;i++){
+System.out.println((char)('A'+i)+" -->"+('A'+i));
+}
+}
+}
